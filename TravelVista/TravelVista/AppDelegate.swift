e5b9918c6c359eab900ebf44764a676c0884e598
invictus841 <usedforfunny@gmail.com>
2025-04-11 05:12:31 +0200

    Changed the initial view to be ListView instead of the main storyboard in AppDelegate

--- TravelVista/TravelVista/AppDelegate.swift
+++ TravelVista/TravelVista/AppDelegate.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: ListView())
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
     }
 }

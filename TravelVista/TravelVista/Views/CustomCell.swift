67692bfa75c11e97b3cc9846e8f8f1a43f5254ab
invictus841 <usedforfunny@gmail.com>
2025-04-11 05:13:42 +0200

    Removed CustomCell, we have CountryRow view in SwiftUI now

--- TravelVista/TravelVista/Views/CustomCell.swift
+++ TravelVista/TravelVista/Views/CustomCell.swift
@@ -5,24 +5,24 @@
 //  Created by Amandine Cousin on 18/12/2023.
 //
 
-import UIKit
-
-class CustomCell: UITableViewCell {
-    @IBOutlet weak var rateLabel: UILabel!
-    @IBOutlet weak var capitalLabel: UILabel!
-    @IBOutlet weak var countryImageView: UIImageView!
-    @IBOutlet weak var countryNameLabel: UILabel!
-    
-    var country: Country?
-    
-    // Fonction qui permet de configurer la cellule en fonction d'un Country
-    func setUpCell(country: Country){
-        self.countryImageView.layer.cornerRadius = self.countryImageView.frame.size.width / 2
-        
-        self.country = country
-        self.countryNameLabel.text = country.name
-        self.capitalLabel.text = country.capital
-        self.countryImageView.image = UIImage(named: country.pictureName)
-        self.rateLabel.text = String(country.rate)
-    }
-}
+//import UIKit
+//
+//class CustomCell: UITableViewCell {
+//    @IBOutlet weak var rateLabel: UILabel!
+//    @IBOutlet weak var capitalLabel: UILabel!
+//    @IBOutlet weak var countryImageView: UIImageView!
+//    @IBOutlet weak var countryNameLabel: UILabel!
+//    
+//    var country: Country?
+//    
+//    // Fonction qui permet de configurer la cellule en fonction d'un Country
+//    func setUpCell(country: Country){
+//        self.countryImageView.layer.cornerRadius = self.countryImageView.frame.size.width / 2
+//        
+//        self.country = country
+//        self.countryNameLabel.text = country.name
+//        self.capitalLabel.text = country.capital
+//        self.countryImageView.image = UIImage(named: country.pictureName)
+//        self.rateLabel.text = String(country.rate)
+//    }
+//}

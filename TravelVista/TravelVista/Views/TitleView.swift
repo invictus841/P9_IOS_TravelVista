abcd1d5ed10d20dca5cfb18bede7f1fc74bffed0
invictus841 <usedforfunny@gmail.com>
2025-04-11 04:57:15 +0200

    TitleView modified

--- TravelVista/TravelVista/Views/TitleView.swift
+++ TravelVista/TravelVista/Views/TitleView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct TitleView: View {
+    var countryName: String
+    var capitalName: String
+    var rate: Int
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(countryName)
+                    .font(.headline)
+                    .foregroundColor(.blue)
+
+                Text(capitalName)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+
+            Spacer()
+
+            HStack(spacing: 2) {
+                ForEach(0..<rate, id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.orange)
+                }
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
     }
 }
 
+
 #Preview {
-    TitleView()
+    TitleView(countryName: "Vietnam", capitalName: "Hanoi", rate: 5)
 }
